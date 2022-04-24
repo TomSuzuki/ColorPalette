12a50d533d506ac47110acccba741961dbdc55f8
TomSuzuki <tomsuzuki2633@gmail.com>
2022-04-24 17:24:06 +0900

    #8 test

---
+++ components/Main.vue
@@ -0,0 +1,14 @@
+<!-- top page -->
+
+<template>
+  <div class="index">
+    <Title/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Main'
+}
+</script>
+
---
+++ components/Title.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="outer">
+    <h1>Color Pallet</h1>
+    <small>ver 1.22</small>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Title",
+};
+</script>
+
+<style>
+.outer {
+  grid-area: head;
+  padding: 1.5em 1em;
+  margin-bottom: 1em;
+  background-color: #111111;
+  color: #efefef;
+}
+
+h1 {
+  padding: 0 0.5em 0.4em;
+  margin: 0;
+}
+</style>
