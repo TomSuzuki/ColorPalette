f1554d0941bb7c527ced86c17dabe8ea3e45b22d
TomSuzuki <tomsuzuki2633@gmail.com>
2022-04-24 23:43:45 +0900

    json

--- components/Pallet.vue
+++ components/Pallet.vue
@@ -1,6 +1,14 @@
 <template>
   <div class="pallet">
-    {{ color }}
+    <ul v-for="c in color" :key="c">
+      <li>
+        <h3>{{ c.name }}</h3>
+        <h4>{{ c.sub_name }}</h4>
+        <ul v-for="rgb in c.color">
+          <li>{{ rgb }}</li>
+        </ul>
+      </li>
+    </ul>
   </div>
 </template>
 
@@ -11,7 +19,7 @@ export default {
   name: "Pallet",
   data() {
     return {
-      color: color,
+      color: color["color"],
     };
   },
 };
